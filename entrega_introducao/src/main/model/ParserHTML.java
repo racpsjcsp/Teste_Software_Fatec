0321687ff1476dd69e7ca037eeeef971cd11855e
Rafael Plinio <racpsjcsp@gmail.com>
2020-05-18 22:41:30 -0300

    JSoup
    HTML Parser

---
+++ entrega_introducao/src/main/model/ParserHTML.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class ParserHTML {
+    public ArrayList<String> parseFromWiki() {
+    	
+    	//classe -- objeto
+    	Document doc;
+    	ArrayList<String> lista = new ArrayList<String>();
+		try {
+			
+			//site
+			doc = Jsoup.connect("https://en.wikipedia.org/").get();
+			
+			//seleciona id 
+	    	Elements newsHeadlines = doc.select("#mp-itn b a");
+	    	
+	    	//pega as tags "title" e guarda na lista 
+	    	for (Element headline : newsHeadlines) {
+	    		lista.add(headline.attr("title"));
+	    	}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+        return lista;
+    }
+}
