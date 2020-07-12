7e9c15c0035c56f3798294a76521d557d93c47df
Rafael Plinio <racpsjcsp@gmail.com>
2020-07-12 10:09:37 -0300

    AluguelTest e TesteParametrizado update
    Add Valores limites do teste nominal e automatização do teste, sem ter que digitar os dias desejados manualmente.
    
    Add correção de alguns valores limites no CsvSource e CsvFile

--- entrega_testes_funcionais/teste_api_aluguel/src/test/java/model/AluguelTest.java
+++ entrega_testes_funcionais/teste_api_aluguel/src/test/java/model/AluguelTest.java
@@ -12,94 +12,88 @@ public class AluguelTest {
 	int dia;
 
     @Test public void testNominalRACP1() throws JSONException {
-        Aluguel alug = new Aluguel();
-        
-        dia = 2; //0,1,2 
+        Aluguel alug = new Aluguel();      
 
-        if (dia == 1 || dia == 2) {
+        for (int dia = 0; dia <= 2; dia++) {
         	Double testNominalRACP1 = valor-(valor*0.10); //dia 1 ao 5 -> -10%  621.00
-         	JSONObject result = alug.inputAluguelDia(valor,dia);
-         	System.out.println(result);
-            assertEquals(testNominalRACP1, result.getDouble("valor_calculado"), 0.01);
-        } else if (dia < 0) {
-        	int testNominalRACP1 = -1; //dia < 0	    	
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP1, result.getDouble("valor_calculado"), 0.01);
-        }
+            JSONObject result = alug.inputAluguelDia(valor,dia);
+            assertEquals(testNominalRACP1, result.getDouble("valor_calculado"), 0.01);           
+        }  
     }
        
     @Test public void testNominalRACP2() throws JSONException {
         Aluguel alug = new Aluguel();
         
-        dia = 6; //4,5,6
-
-        if (dia == 4 || dia == 5) {      	
-        	Double testNominalRACP2 = valor-(valor*0.10); //dia 9 -> -5,14% aprox
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP2, result.getDouble("valor_calculado"),0.01);
-	    } else if (dia == 6) {
-	    	Double testNominalRACP2 = valor-(valor*0.05); //dia 6 ao 10 -> -5%
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP2, result.getDouble("valor_calculado"),0.01);
-	    }
+        for (int dia = 4; dia <= 6; dia++) {    	
+        	if (dia == 4 || dia == 5) {
+        		Double testNominalRACP2 = valor-(valor*0.10); //dia 1 ao 5 -> -10%  621.00
+                JSONObject result = alug.inputAluguelDia(valor,dia);
+                assertEquals(testNominalRACP2, result.getDouble("valor_calculado"), 0.01);
+        	} else if (dia == 6) {
+        		Double testNominalRACP2 = valor-(valor*0.05);
+                JSONObject result = alug.inputAluguelDia(valor,dia);
+                assertEquals(testNominalRACP2, result.getDouble("valor_calculado"), 0.01);
+        	}        	
+        }             
     }
     
     @Test public void testNominalRACP3() throws JSONException {
         Aluguel alug = new Aluguel();
-        
-        dia = 11; //9,10,11
 
-        if (dia == 9) {      	
-        	Double testNominalRACP3 = valor-35.5; //dia 15  -> -10%
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP3, result.getDouble("valor_calculado"),0.01);
-	    } else if (dia == 11 || dia == 10) {
-	    	Double testNominalRACP3 = valor; //dia 11 ao 14 -> 0%
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP3, result.getDouble("valor_calculado"),0.01);
-	    }
+        for (int dia = 9; dia <= 11; dia++) {        	
+        	if (dia == 9 ) {
+        		Double testNominalRACP3 = valor-35.5;
+                JSONObject result = alug.inputAluguelDia(valor,dia);                
+        		assertEquals(testNominalRACP3, result.getDouble("valor_calculado"), 0.01);   
+        	} else if (dia == 10 ) {
+        		Double testNominalRACP3 = valor-(valor*0.05);
+        		JSONObject result = alug.inputAluguelDia(valor,dia);        		
+        		assertEquals(testNominalRACP3, result.getDouble("valor_calculado"), 0.01);
+        	} else if (dia == 11) {
+        		Double testNominalRACP3 = valor;
+        		JSONObject result = alug.inputAluguelDia(valor,dia);
+        		assertEquals(testNominalRACP3, result.getDouble("valor_calculado"), 0.01);
+        	}  
+        }
     }
     
     @Test public void testNominalRACP4() throws JSONException {
         Aluguel alug = new Aluguel();
         
-        dia = 16; //14,15,16...29,30,31
-
-        if (dia == 16) {      	
-        	Double testNominalRACP4 = valor+(valor*0.021); //dia 16 -> +2,1%
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	     } else if (dia == 15) {
-	     	Double testNominalRACP4 = valor+(valor*0.10); //dia 15
-	     	JSONObject result = alug.inputAluguelDia(valor,dia);
-	     	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	     } else if (dia == 14) {
-	     	Double testNominalRACP4 = valor; //dia 14 0%
-	     	JSONObject result = alug.inputAluguelDia(valor,dia);
-	     	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	     } else if (dia == 29) {
-	     	Double testNominalRACP4 = valor+(valor*0.034); //dia 29 -> +3,4%
-	     	JSONObject result = alug.inputAluguelDia(valor,dia);
-	     	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	    } else if (dia == 30) {
-	    	Double testNominalRACP4 = valor+(valor*0.035); //dia 30 -> +3,5%
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	    } else if (dia >= 31) {	    	
-	    	int testNominalRACP4 = -1; //dia >= 31	    	
-	    	JSONObject result = alug.inputAluguelDia(valor,dia);
-	    	System.out.println(result);
-	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
-	    }
-    }    
+        for (int dia = 14; dia <= 16; dia++) {
+        	if (dia == 16) {      	
+            	Double testNominalRACP4 = valor+(valor*0.021); //dia 16 -> +2,1%
+    	    	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
+    	     } else if (dia == 15) {
+    	     	Double testNominalRACP4 = valor-(valor*0.10); //dia 15
+    	     	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
+    	     } else if (dia == 14) {
+    	     	Double testNominalRACP4 = valor; //dia 14 0%
+    	     	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP4, result.getDouble("valor_calculado"),0.01);
+    	     }
+        }
+    } 
+    
+    @Test public void testNominalRACP5() throws JSONException {
+    	Aluguel alug = new Aluguel();
+    	
+    	for (int dia = 29; dia <= 31; dia++) {
+    		if (dia == 29) {
+    			Double testNominalRACP5 = valor+(valor*0.034);
+    	    	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP5, result.getDouble("valor_calculado"),0.01);
+    		} else if (dia == 30) {
+    			Double testNominalRACP5 = valor+(valor*0.035);
+    	    	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP5, result.getDouble("valor_calculado"),0.01);
+    		} else if (dia <= 31) {
+    			int testNominalRACP5 = -1;
+    	    	JSONObject result = alug.inputAluguelDia(valor,dia);
+    	        assertEquals(testNominalRACP5, result.getDouble("valor_calculado"),0.01);
+    		}
+    	}
+    }
 }
--- entrega_testes_funcionais/teste_api_aluguel/src/test/java/model/TesteParametrizado.java
+++ entrega_testes_funcionais/teste_api_aluguel/src/test/java/model/TesteParametrizado.java
@@ -11,28 +11,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TesteParametrizado {
 	Aluguel alug = new Aluguel();
 	
-	@Disabled
+	//@Disabled
     @ParameterizedTest 
-    @CsvSource(value = {										//dias
-    		"690,-1,-1","690,0,621","690,1,621",				//0,1,2
-    		"690,5,621","690,6,655.5","690,7,655.5",			//4,5,6
-    		"690,9,654.5","690,10,655.5","690,11,690",			//9,10,11
-    		"690,14,690","690,15,621","690,16,704.49",			//14,15,16
-    		"690,29,713.46","690,30,714.15","690,31,-1",},		//29,30,31
-    		delimiter = ',') //1
+    @CsvSource(value = {											//dias
+    		"690,0,621","690,1,621","690,2,621",					//0,1,2
+    		"690,4,621","690,5,621","690,6,655.5",					//4,5,6
+    		"690,9,654.5","690,10,655.5","690,11,690",				//9,10,11
+    		"690,14,690","690,15,621","690,16,704.49",				//14,15,16
+    		"690,29,713.46","690,30,714.15","690,31,-1",},			//29,30,31
+    		delimiter = ',')
     public void testCsvSourceRACP(Double valor, int dia, Double result) throws JSONException {
 		JSONObject obj = alug.inputAluguelDia(valor,dia);		
         assertEquals(result, obj.getDouble("valor_calculado"), 0.01);           
     }
-     
-   
+        
     @ParameterizedTest(name = "Teste {index} => valor={0} dia={1}," + " resultado={2}")
     @CsvFileSource(resources = "/arquivoCSV_1.csv", delimiter = ',')
 	public void testCsvFileSourceRACP(Double valor, int dia, Double result) throws JSONException {
     	JSONObject obj = alug.inputAluguelDia(valor,dia);		
         assertEquals(result, obj.getDouble("valor_calculado"), 0.01);       
 	}
-    
-  
 }
 
